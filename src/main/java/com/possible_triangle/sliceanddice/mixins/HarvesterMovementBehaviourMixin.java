de5064be8f39ea81efceb23555b0a51db2bdab4c
Niklas Widmann <nik@somethingcatchy.net>
2024-04-17 09:38:32 +0200

    1.19 uses old fabric port still

--- src/main/java/com/possible_triangle/sliceanddice/mixins/HarvesterMovementBehaviourMixin.java
+++ src/main/java/com/possible_triangle/sliceanddice/mixins/HarvesterMovementBehaviourMixin.java
@@ -1,5 +1,6 @@
 package com.possible_triangle.sliceanddice.mixins;
 
+import com.nhoryzon.mc.farmersdelight.registry.ItemsRegistry;
 import com.possible_triangle.sliceanddice.compat.ModCompat;
 import com.simibubi.create.content.contraptions.actors.harvester.HarvesterMovementBehaviour;
 import net.minecraft.world.item.ItemStack;
@@ -7,13 +8,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import vectorwing.farmersdelight.common.registry.ModItems;
 
 @Mixin(HarvesterMovementBehaviour.class)
 public class HarvesterMovementBehaviourMixin {
 
     @Unique
-    private static final ItemStack sliceanddice$TOOL = ModCompat.INSTANCE.ifLoaded(ModCompat.FARMERS_DELIGHT, () -> new ItemStack(ModItems.IRON_KNIFE.get()));
+    private static final ItemStack sliceanddice$TOOL = ModCompat.INSTANCE.ifLoaded(ModCompat.FARMERS_DELIGHT, () -> new ItemStack(ItemsRegistry.IRON_KNIFE.get()));
 
     @ModifyVariable(
             require = 0,
