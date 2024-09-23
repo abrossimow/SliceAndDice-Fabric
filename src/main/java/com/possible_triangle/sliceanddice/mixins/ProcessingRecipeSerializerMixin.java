befe923aaad26ccd89825c75cec3287ab5c9861c
Niklas Widmann <nik@somethingcatchy.net>
2024-09-23 18:23:52 +0200

    fabric mixins actually have to be remapped

--- src/main/java/com/possible_triangle/sliceanddice/mixins/ProcessingRecipeSerializerMixin.java
+++ src/main/java/com/possible_triangle/sliceanddice/mixins/ProcessingRecipeSerializerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = ProcessingRecipeSerializer.class, remap = false)
+@Mixin(ProcessingRecipeSerializer.class)
 public class ProcessingRecipeSerializerMixin {
 
     @Redirect(
